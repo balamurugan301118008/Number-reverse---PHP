d379e9bed346eaad157399bb7398c38ae01ccfea
balamurugan301118008 <balamuruganrdckap@gmail.com>
2023-05-29 12:00:05 +0530

    completed

---
+++ reverse.php
@@ -0,0 +1,18 @@
+<?php
+
+// one of the way 
+function reverse($input)
+{
+    for ($i = count($input); $i >= 0; $i--) {
+        // echo $i."\n";
+        echo $input[$i]."\n";
+    }
+}
+reverse([1, 2, 3, 4, 5]);
+
+echo  "****************************";
+// one of the way 
+$number = array(21,3,2,32,3,434,4);
+print_r(array_reverse($number))."\n";
+// print_r(strrev($number));
+?>
